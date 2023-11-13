23fd140b95dac09734f18a2b5a3a7b418fb975d5
MUHAMMED IRFAN P <112098500+IrfanKpm@users.noreply.github.com>
2023-11-13 23:01:03 +0530

    Add files via upload

---
+++ pycolors.py
@@ -0,0 +1,36 @@
+
+
+# Light colors with bold and without bold
+
+BB = "\033[34;1m"  # Blue light with bold
+YY = "\033[33;1m"  # Yellow light with bold
+GG = "\033[32;1m"  # Green light with bold
+WW = "\033[0;1m"   # White light with bold
+RR = "\033[31;1m"  # Red light with bold
+CC = "\033[36;1m"  # Cyan light with bold
+PP = "\033[35;1m"  # Purple light with bold
+OO = "\033[38;5;208;1m"  # Orange light with bold
+
+# Regular colors
+B = "\033[34m"     # Blue without bold
+Y = "\033[33m"     # Yellow without bold
+G = "\033[32m"     # Green without bold
+W = "\033[0m"      # White without bold
+R = "\033[31m"     # Red without bold
+C = "\033[36m"     # Cyan without bold
+P = "\033[35m"     # Purple without bold
+O = "\033[38;5;208m"  # Orange without bold
+
+# Background colors
+BG_B = "\033[44m"  # Blue background
+BG_Y = "\033[43m"  # Yellow background
+BG_G = "\033[42m"  # Green background
+BG_W = "\033[47m"  # White background
+BG_R = "\033[41m"  # Red background
+BG_C = "\033[46m"  # Cyan background
+BG_P = "\033[45m"  # Purple background
+BG_O = "\033[48;5;208m"  # Orange background
+
+# Additional styles
+UNDERLINE = "\033[4m"  # Underlined text
+BLINK = "\033[5m"      # Blinking text
